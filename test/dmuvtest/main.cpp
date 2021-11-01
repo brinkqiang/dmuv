c74d92f536020e55116265e917be1ab7859ba9f9
brinkqiang <brink.qiang@gmail.com>
2021-11-01 20:26:19 +0800

    add

--- test/dmuvtest/main.cpp
+++ test/dmuvtest/main.cpp
@@ -41,45 +41,52 @@ int main()
         std::cout << (ip4 ? "IP4" : "IP6") << ": " << addr << std::endl;
     });
 
-   uvpp::Tcp tcp(loop);
-   if (!tcp.bind("0.0.0.0", 33013))
-   {
-       return 0;
-   }
+    uvpp::Tcp tcp(loop);
+    if (!tcp.bind("0.0.0.0", 33013))
+    {
+        return 0;
+    }
 
-   tcp.listen(myerror, 5);
+    tcp.listen(myerror, 5);
 
-   uvpp::Tcp client(loop);
-   tcp.accept(client);
+    uvpp::Tcp client(loop);
+    tcp.accept(client);
 
-   client.read_start(myread);
+    client.read_start(myread);
 
-   uvpp::Tcp tcp2(loop);
-   if (!tcp2.connect("127.0.0.1", 33013, [](auto e) {
-       std::cout << "connected: " << e.str() << std::endl;
-   }))
-   {
-       std::cout << "error connect\n";
-   }
-
-   std::thread t([&loop]()
-   {
-      std::cout << "Thread started: "<< std::this_thread::get_id() << std::endl;
-      try
-      {
-        if (!loop.run())
+    uvpp::Tcp tcp2(loop);
+    if (!tcp2.connect("127.0.0.1", 33013, [](auto e) {
+        if (!e)
         {
-            std::cout << "error run\n";
+            std::cout << "connected: " << std::endl;
         }
-        std::cout << "Quit from event loop" << std::endl;
-      }
-      catch(...)
-      {
-          std::cout << "exception\n";
-      }
-   });
+        else
+        {
+            std::cout << "connected failed: " << e.str() << std::endl;
+        }
+    }))
+    {
+        std::cout << "error connect\n";
+    }
 
-   t.join();
+    std::thread t([&loop]()
+    {
+        std::cout << "Thread started: " << std::this_thread::get_id() << std::endl;
+        try
+        {
+            if (!loop.run())
+            {
+                std::cout << "error run\n";
+            }
+            std::cout << "Quit from event loop" << std::endl;
+        }
+        catch (...)
+        {
+            std::cout << "exception\n";
+        }
+    });
 
-   return 0;
+    t.join();
+
+    return 0;
 }
